20f9fc4c83fb9236e5a0c99e941da774ccd22d7e
neo.xu <neo.xu@modooplay.com>
2021-02-22 10:02:46 +0800

    init

---
+++ Assets/Mono/Mono.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mono : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.LogError("Awake");
+    }
+
+    private void OnEnable()
+    {
+        Debug.LogError("OnEnable");
+    }
+
+    private void Start()
+    {
+        Debug.LogError("Start");
+    }
+
+    private void Update()
+    {
+        Debug.LogError("Update");
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.LogError("FixedUpdate");
+    }
+
+    private void LateUpdate()
+    {
+        Debug.LogError("LateUpdate");
+    }
+
+    private void OnDisable()
+    {
+        Debug.LogError("OnDisable");
+    }
+}
