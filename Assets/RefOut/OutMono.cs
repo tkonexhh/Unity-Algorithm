2956138ab4ebcb9e97dafb53cd1ec8476aab2cf1
neo.xu <neo.xu@modooplay.com>
2021-02-25 17:52:03 +0800

    ref out

---
+++ Assets/RefOut/OutMono.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutMono : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int i;
+        DemoOut(out i);
+    }
+
+    private void DemoOut(out int n)
+    {
+        n = 10;
+    }
+
+    // private void DemoOutChange(out int n, out int m)
+    // {
+    //     int temp = n;
+    //     n = m;
+    //     m = temp;
+
+    // }
+}
---
+++ Assets/RefOut/RefMono.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RefMono : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int i = 0;
+        DemoRef(ref i);
+
+        int n = 1, m = 2;
+        DemoRefChange(ref n, ref m);
+        Debug.LogError(n + "--" + m);
+    }
+
+    private void DemoRef(ref int n)
+    {
+        Debug.Log(n);
+    }
+
+    private void DemoRefChange(ref int n, ref int m)
+    {
+        int temp = n;
+        n = m;
+        m = temp;
+
+    }
+}
